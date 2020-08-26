d957e4aeba4fc7527a4b3179d92f50dcfc8a3ec4
abueavan <eavanzhou@gmail.com>
2020-08-26 23:20:29 +0800

    port

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const SSLPORT = 18082
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen = app.listen(5000, function() {
   const { address, port } = server.address()
-  console.log('Http Server is running on http://%s:%s', address, port)
+  console.log(`Http Server is running on http://${address}:${port}`)
 })
 
 httpsServer.listen(SSLPORT, function() {
